c9a317ecd2bf12daa2710734da21651ec84191cf
caspertu <caspertu@gmail.com>
2019-04-08 18:27:01 +0800

    add html

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,13 +2,22 @@ var exec = require('child_process').exec;
 
 function start(res) {
     console.log('Request handler start was called.')
-    var content = 'empty';
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
 
-    exec('find /', function(error, stdout, stderr) {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(stdout);
-        res.end();
-    });
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
 }
 
 function upload() {
