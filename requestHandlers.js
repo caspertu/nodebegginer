f80f7c381347f2c60c38e96016d75304b444b3fe
caspertu <caspertu@gmail.com>
2019-04-08 18:55:17 +0800

    add postData

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,30 +1,34 @@
-var exec = require('child_process').exec;
+var querystring = require("querystring"),
+    fs = require('fs');
 
-function start(res) {
-    console.log('Request handler start was called.')
+function start(response, postData) {
+    console.log('Request handler \'start\' was called.')
+    
     var body = '<html>'+
-        '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
-        '</form>'+
-        '</body>'+
-        '</html>';
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(body);
-    res.end();
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload() {
-    console.log('Request handler upload was called.')
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('UPLOAD');
-    res.end();
+function upload(response, postData) {
+    console.log('Request handler \'upload\' was called. postData is \'' 
+        + postData + '\'');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You've sent:" 
+        + querystring.parse(postData).text);
+    response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -2,16 +2,28 @@ var http = require('http');
 var url = require('url');
 
 function start(route, handle) {
-  const port = 8080;
-  function onRequest(req, res) {
-    var pathname = url.parse(req.url).pathname;
-    console.log('Request for ' + pathname + ' received.')
+    const port = 8080;
+    function onRequest(request, response) {
+      var postData = '';
+      var pathname = url.parse(request.url).pathname;
+      console.log('Request for ' + pathname + ' received.')
 
-    route(handle, pathname, res);
-  }
+      request.setEncoding('utf8');
 
-  http.createServer(onRequest).listen(port);　
-  console.log('Server listening on: http://localhost:%s', port);
+      request.addListener('data', function(postDataChunk) {
+        postData += postDataChunk;
+        console.log('Received POST data chunk "' 
+          + postDataChunk + '".');
+      });
+
+      request.addListener('end', function() {
+        console.log('end');
+        route(handle, pathname, response, postData);
+      });
+    }
+
+    http.createServer(onRequest).listen(port);　
+    console.log('Server listening on: http://localhost:%s', port);
 }
 
 exports.start = start;
